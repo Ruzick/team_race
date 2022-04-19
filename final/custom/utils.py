e1f99971bf734be915e89519c1e610b825ee9150
AH-73 <93234736+AH-73@users.noreply.github.com>
2022-04-19 19:02:41 +0800

    Update utils.py

--- final/custom/utils.py
+++ final/custom/utils.py
@@ -1,204 +1,146 @@
 import numpy as np
-import pystk
-
-from torch.utils.data import Dataset, DataLoader
-import torchvision.transforms.functional as TF
-from . import dense_transforms
-
-RESCUE_TIMEOUT = 30
-TRACK_OFFSET = 15
-DATASET_PATH = 'drive_data'
+from enum import IntEnum
 
 
-class SuperTuxDataset(Dataset):
-    def __init__(self, dataset_path=DATASET_PATH, transform=dense_transforms.ToTensor()):
-        from PIL import Image
-        from glob import glob
-        from os import path
-        self.data = []
-        for f in glob(path.join(dataset_path, '*.csv')):
-            i = Image.open(f.replace('.csv', '.png'))
-            i.load()
-            self.data.append((i, np.loadtxt(f, dtype=np.float32, delimiter=',')))
-        self.transform = transform
-
-    def __len__(self):
-        return len(self.data)
-
-    def __getitem__(self, idx):
-        data = self.data[idx]
-        data = self.transform(*data)
-        return data
+class Team(IntEnum):
+    RED = 0
+    BLUE = 1
 
 
-def load_data(dataset_path=DATASET_PATH, transform=dense_transforms.ToTensor(), num_workers=0, batch_size=128):
-    dataset = SuperTuxDataset(dataset_path, transform=transform)
-    return DataLoader(dataset, num_workers=num_workers, batch_size=batch_size, shuffle=True, drop_last=True)
+def video_grid(team1_images, team2_images, team1_state='', team2_state=''):
+    from PIL import Image, ImageDraw
+    grid = np.hstack((np.vstack(team1_images), np.vstack(team2_images)))
+    grid = Image.fromarray(grid)
+    grid = grid.resize((grid.width // 2, grid.height // 2))
+
+    draw = ImageDraw.Draw(grid)
+    draw.text((20, 20), team1_state, fill=(255, 0, 0))
+    draw.text((20, grid.height // 2 + 20), team2_state, fill=(0, 0, 255))
+    return grid
 
 
-class PyTux:
-    _singleton = None #allows you to create just one instance of a class
-                      #leading underscore just means the variable is private in python 
+def map_image(team1_state, team2_state, soccer_state, resolution=512, extent=65, anti_alias=1):
+    BG_COLOR = (0xee, 0xee, 0xec)
+    RED_COLOR = (0xa4, 0x00, 0x00)
+    BLUE_COLOR = (0x20, 0x4a, 0x87)
+    BALL_COLOR = (0x2e, 0x34, 0x36)
+    from PIL import Image, ImageDraw
+    r = Image.new('RGB', (resolution*anti_alias, resolution*anti_alias), BG_COLOR)
 
-    def __init__(self, screen_width=128, screen_height=96):
-        assert PyTux._singleton is None, "Cannot create more than one pytux object"
-        PyTux._singleton = self
-        self.config = pystk.GraphicsConfig.hd()  #configure supertuxkart
-        self.config.screen_width = screen_width
-        self.config.screen_height = screen_height
-        pystk.init(self.config)
-        self.k = None   #what is k??
+    def _to_coord(x):
+        return resolution * anti_alias * (x + extent) / (2 * extent)
+
+    draw = ImageDraw.Draw(r)
+    # Let's draw the goal line
+    draw.line([(_to_coord(x), _to_coord(y)) for x, _, y in soccer_state['goal_line'][0]], width=5*anti_alias, fill=RED_COLOR)
+    draw.line([(_to_coord(x), _to_coord(y)) for x, _, y in soccer_state['goal_line'][1]], width=5*anti_alias, fill=BLUE_COLOR)
+
+    # and the ball
+    x, _, y = soccer_state['ball']['location']
+    s = soccer_state['ball']['size']
+    draw.ellipse((_to_coord(x-s), _to_coord(y-s), _to_coord(x+s), _to_coord(y+s)), width=2*anti_alias, fill=BALL_COLOR)
+
+    # and karts
+    for c, s in [(BLUE_COLOR, team1_state), (RED_COLOR, team2_state)]:
+        for k in s:
+            x, _, y = k['kart']['location']
+            fx, _, fy = k['kart']['front']
+            sx, _, sy = k['kart']['size']
+            s = (sx+sy) / 2
+            draw.ellipse((_to_coord(x - s), _to_coord(y - s), _to_coord(x + s), _to_coord(y + s)), width=5*anti_alias, fill=c)
+            draw.line((_to_coord(x), _to_coord(y), _to_coord(x+(fx-x)*2), _to_coord(y+(fy-y)*2)), width=4*anti_alias, fill=0)
+
+    if anti_alias == 1:
+        return r
+    return r.resize((resolution, resolution), resample=Image.ANTIALIAS)
 
 
-    '''
-    @staticmethod  #not accessing the instances or the class
-    def _point_on_track(distance, track, offset=0.0):
-        """
-        Get a point at `distance` down the `track`. Optionally applies an offset after the track segment if found.
-        Returns a 3d coordinate
-        """
-        node_idx = np.searchsorted(track.path_distance[..., 1],
-                                   distance % track.path_distance[-1, 1]) % len(track.path_nodes)
+# Recording functionality
+class BaseRecorder:
+    def __call__(self, team1_state, team2_state, soccer_state, actions, team1_images=None, team2_images=None):
+        raise NotImplementedError
 
-        #path_distance is the distance down the track of each line segment 
-        #path_nodes is the center line of the drivable area as line segments of the 3D coordinate 
-        d = track.path_distance[node_idx]
-        x = track.path_nodes[node_idx]
-        t = (distance + offset - d[0]) / (d[1] - d[0])
-        return x[1] * t + x[0] * (1 - t)
+    def __and__(self, other):
+        return MultiRecorder(self, other)
 
-    '''
+    def __rand__(self, other):
+        return MultiRecorder(self, other)
 
-    @staticmethod
-    def _to_image(x, proj, view): #what is proj
-        p = proj @ view @ np.array(list(x) + [1])
-        return np.clip(np.array([p[0] / p[-1], -p[1] / p[-1]]), -1, 1)
 
-    def rollout(self, track, controller, planner=None, max_frames=1000, verbose=False, data_callback=None):
-        """
-        Play a level (track) for a single round.
-        :param track: Name of the track
-        :param controller: low-level controller, see controller.py
-        :param planner: high-level planner, see planner.py
-        :param max_frames: Maximum number of frames to play for
-        :param verbose: Should we use matplotlib to show the agent drive?
-        :param data_callback: Rollout calls data_callback(time_step, image, 2d_aim_point) every step, used to store the
-                              data
-        :return: Number of steps played
-        """
-        if self.k is not None and self.k.config.track == track:
-            self.k.restart()
-            self.k.step()
+class MultiRecorder(BaseRecorder):
+    def __init__(self, *recorders):
+        self._r = [r for r in recorders if r]
+
+    def __call__(self, *args, **kwargs):
+        for r in self._r:
+            r(*args, **kwargs)
+
+
+class VideoRecorder(BaseRecorder):
+    """
+        Produces pretty output videos
+    """
+    def __init__(self, video_file):
+        import imageio
+        self._writer = imageio.get_writer(video_file, fps=20)
+
+    def __call__(self, team1_state, team2_state, soccer_state, actions, team1_images=None, team2_images=None):
+        if team1_images and team2_images:
+            self._writer.append_data(np.array(video_grid(team1_images, team2_images,
+                                                         'Blue: %d' % soccer_state['score'][1],
+                                                         'Red: %d' % soccer_state['score'][0])))
         else:
-            if self.k is not None:
-                self.k.stop()
-                del self.k
-            config = pystk.RaceConfig(num_kart=1, laps=1, track=track)
-            config.players[0].controller = pystk.PlayerConfig.Controller.PLAYER_CONTROL
+            self._writer.append_data(np.array(map_image(team1_state, team2_state, soccer_state)))
 
-            self.k = pystk.Race(config)
-            self.k.start()
-            self.k.step()
+    def __del__(self):
+        if hasattr(self, '_writer'):
+            self._writer.close()
 
-        state = pystk.WorldState()
-        track = pystk.Track()
-        puck = pystk.SoccerBall()
 
-        last_rescue = 0
+class DataRecorder(BaseRecorder):
+    def __init__(self, record_images=False):
+        self._record_images = record_images
+        self._data = []
 
-        for t in range(max_frames):
+    def __call__(self, team1_state, team2_state, soccer_state, actions, team1_images=None, team2_images=None):
+        data = dict(team1_state=team1_state, team2_state=team2_state, soccer_state=soccer_state, actions=actions)
+        if self._record_images:
+            data['team1_images'] = team1_images
+            data['team2_images'] = team2_images
+        self._data.append(data)
 
-            state.update()
-            track.update()
-            puck.update()
+    def data(self):
+        return self._data
 
-            kart = state.players[0].kart
+    def reset(self):
+        self._data = []
 
-            if np.isclose(kart.overall_distance / track.length, 1.0, atol=2e-3):
-                if verbose:
-                    print("Finished at t=%d" % t)
+
+class StateRecorder(BaseRecorder):
+    def __init__(self, state_action_file, record_images=False):
+        self._record_images = record_images
+        self._f = open(state_action_file, 'wb')
+
+    def __call__(self, team1_state, team2_state, soccer_state, actions, team1_images=None, team2_images=None):
+        from pickle import dump
+        data = dict(team1_state=team1_state, team2_state=team2_state, soccer_state=soccer_state, actions=actions)
+        if self._record_images:
+            data['team1_images'] = team1_images
+            data['team2_images'] = team2_images
+        dump(dict(data), self._f)
+        self._f.flush()
+
+    def __del__(self):
+        if hasattr(self, '_f'):
+            self._f.close()
+
+
+def load_recording(recording):
+    from pickle import load
+    with open(recording, 'rb') as f:
+        while True:
+            try:
+                yield load(f)
+            except EOFError:
                 break
 
-            proj = np.array(state.players[0].camera.projection).T
-            view = np.array(state.players[0].camera.view).T
-
-            #aim_point_world = self._point_on_track(kart.distance_down_track+TRACK_OFFSET, track)
-            aim_point_world=puck.ball()
-            aim_point_image = self._to_image(aim_point_world, proj, view)
-            if data_callback is not None:
-                data_callback(t, np.array(self.k.render_data[0].image), aim_point_image)
-
-            if planner:
-                image = np.array(self.k.render_data[0].image)
-                aim_point_image = planner(TF.to_tensor(image)[None]).squeeze(0).cpu().detach().numpy()
-
-            current_vel = np.linalg.norm(kart.velocity)
-            action = controller(aim_point_image, current_vel)
-
-            if current_vel < 1.0 and t - last_rescue > RESCUE_TIMEOUT:
-                last_rescue = t
-                action.rescue = True
-
-            self.k.step(action)
-            t += 1
-        return t, kart.overall_distance / track.length
-
-    def close(self):
-        """
-        Call this function, once you're done with PyTux
-        """
-        if self.k is not None:
-            self.k.stop()
-            del self.k
-        pystk.clean()
-
-
-if __name__ == '__main__':
-    from .controller import control
-    from argparse import ArgumentParser
-    from os import makedirs
-
-
-    def noisy_control(aim_pt, vel):
-        return control(aim_pt + np.random.randn(*aim_pt.shape) * aim_noise,
-                       vel + np.random.randn() * vel_noise)
-
-
-    parser = ArgumentParser("Collects a dataset for the high-level planner")
-    parser.add_argument('track', nargs='+')
-    parser.add_argument('-o', '--output', default=DATASET_PATH)
-    parser.add_argument('-n', '--n_images', default=10000, type=int)
-    parser.add_argument('-m', '--steps_per_track', default=20000, type=int)
-    parser.add_argument('--aim_noise', default=0.1, type=float)
-    parser.add_argument('--vel_noise', default=5, type=float)
-    parser.add_argument('-v', '--verbose', action='store_true')
-    args = parser.parse_args()
-    try:
-        makedirs(args.output)
-    except OSError:
-        pass
-    pytux = PyTux()
-    for track in args.track:
-        n, images_per_track = 0, args.n_images // len(args.track)
-        aim_noise, vel_noise = 0, 0
-
-
-        def collect(_, im, pt):
-            from PIL import Image
-            from os import path
-            global n
-            id = n if n < images_per_track else np.random.randint(0, n + 1)
-            if id < images_per_track:
-                fn = path.join(args.output, track + '_%05d' % id)
-                Image.fromarray(im).save(fn + '.png')
-                with open(fn + '.csv', 'w') as f:
-                    f.write('%0.1f,%0.1f' % tuple(pt))
-            n += 1
-
-
-        while n < args.steps_per_track:
-            steps, how_far = pytux.rollout(track, noisy_control, max_frames=1000, verbose=args.verbose, data_callback=collect)
-            print(steps, how_far)
-            # Add noise after the first round
-            aim_noise, vel_noise = args.aim_noise, args.vel_noise
-    pytux.close()
