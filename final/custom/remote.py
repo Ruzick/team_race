2e516cbe96f3e1d7c75e63d6cee228a214cf472a
Mila <jarmila.ruzicka0@gmail.com>
2022-04-22 10:30:59 -0400

    added model, need to test in training

---
+++ final/custom/remote.py
@@ -0,0 +1,34 @@
+
+from . import utils
+from . import  runner
+# TODO: Wrap TeamRunner and Team in ray if possible
+try:
+    import ray
+
+    @ray.remote
+    class RayMatch(runner.Match):
+        pass
+
+    @ray.remote
+    class RayTeamRunner(runner.TeamRunner):
+        pass
+
+    @ray.remote
+    class RayStateRecorder(utils.StateRecorder):
+        pass
+
+    @ray.remote
+    class RayDataRecorder(utils.DataRecorder):
+        pass
+
+    @ray.remote
+    class RayVideoRecorder(utils.VideoRecorder):
+        pass
+
+    RayMatchException = ray.exceptions.RayTaskError
+
+    get = ray.get
+    init = ray.init
+
+except ImportError:
+    ray = None
